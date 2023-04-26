cae2f0e765b124d52489ca562002ce28b10038cc
halangbacca <halanbacca@outlook.com>
2023-04-26 14:42:30 -0300

    Criação de um formulário para cadastro de novos produtos e endpoint para exibir todos os produtos

--- src/main/java/com/example/thymeleaf/controller/ProdutoController.java
+++ src/main/java/com/example/thymeleaf/controller/ProdutoController.java
@@ -5,30 +5,62 @@ import com.example.thymeleaf.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/produto")
+@RequestMapping("/formulario")
 public class ProdutoController {
     @Autowired
     private ProdutoService service;
 
     @GetMapping
-    public String getProduto(Model model) {
-        model.addAttribute("nome", "Xiaomi Redmi Note 8 Pro");
-        model.addAttribute("descricao", "Smartphone intermediário premium");
-        model.addAttribute("dataLancamento", "2019");
-        model.addAttribute("valor", "R$ 2.000");
+    public String chamaFormulario(Produto produto, Model model) {
+        return "formulario";
+    }
 
-        Produto produto = service.findById(1L);
-        produto.setNome("Xiaomi Redmi Note 8 Pro");
-        produto.setDescricao("Smartphone intermediário premium");
-        produto.setDataLancamento("2019");
-        produto.setValor(1800.00);
+//    @GetMapping
+//    public String getProduto(Model model) {
+//        model.addAttribute("nome", "Xiaomi Redmi Note 8 Pro");
+//        model.addAttribute("descricao", "Smartphone intermediário premium");
+//        model.addAttribute("dataLancamento", "2019");
+//        model.addAttribute("valor", "R$ 2.000");
+//
+//        Produto produto = service.findById(1L);
+//        produto.setNome("Xiaomi Redmi Note 8 Pro");
+//        produto.setDescricao("Smartphone intermediário premium");
+//        produto.setDataLancamento("2019");
+//        produto.setValor(1800.00);
+//
+//        model.addAttribute("produto", produto);
+//        return "lista_produtos";
+//    }
 
-        model.addAttribute("produto", produto);
-        return "produto";
+    @GetMapping("/lista")
+    public String listarProdutos(Model model) {
+
+        List<Produto> produtos = service.findAll();
+
+        model.addAttribute("produtos", produtos);
+
+        return "lista_produtos";
+    }
+
+    @PostMapping
+    public String postFormulario(@Validated Produto novoProduto, Model model) {
+        Produto produto = new Produto();
+        produto.setNome(novoProduto.getNome());
+        produto.setDescricao(novoProduto.getDescricao());
+        produto.setDataLancamento(novoProduto.getDataLancamento());
+        produto.setValor(novoProduto.getValor());
+
+        service.save(produto);
+
+        return "redirect:formulario/lista";
     }
 
 }
